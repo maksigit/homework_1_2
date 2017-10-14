327b351c8030808f9741a80e1707b851d1dde016
maksigit <maski123p@gmail.com>
2017-10-14 13:28:12 +0300

    Commit Homework_1_2

---
+++ homework_1_2_symfony/README.md
@@ -0,0 +1,4 @@
+homework_1_2_symfony
+====================
+
+A Symfony project created on October 13, 2017, 4:10 pm.
---
+++ homework_1_2_symfony/src/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkholod
+ * Date: 10/13/17
+ * Time: 7:38 PM
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new \Symfony\Component\Stopwatch\Stopwatch();
+
